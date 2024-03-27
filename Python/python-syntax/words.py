26a8405a0b3cdf48aca88d1d7cf3fe8d2c4f5ed8
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2024-03-27 12:06:06 -0500

    compeleted part 2 word.py

---
+++ Python/python-syntax/words.py
@@ -0,0 +1,24 @@
+def print_words(words):
+    
+    for word in words:
+  
+     if "e" in word:
+         print (word.upper())
+    
+   
+     
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+print_words(["hello", "hey", "goodbye", "yo", "yes"])
