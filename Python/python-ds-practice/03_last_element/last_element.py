ce8d43ceef7452c8efda90f250e1369b7b68b450
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2024-03-30 13:08:38 -0500

    finished

---
+++ Python/python-ds-practice/03_last_element/last_element.py
@@ -0,0 +1,16 @@
+def last_element(lst):
+    """Return last item in list (None if list is empty.
+    
+        >>> last_element([1, 2, 3])
+        3
+        
+        >>> last_element([]) is None
+        True
+    """
+    return lst[-1]
+
+    if lst == 0 :
+        return None
+    
+    
+print(last_element([1,2,3,4,5,6,7]))
