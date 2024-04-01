8cbbcff2f4c6dff46d803cbb3f68dac95733cfba
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2024-04-01 16:29:55 -0500

    finished python 00

--- Python/python-syntax/count_up.py
+++ Python/python-syntax/count_up.py
@@ -16,4 +16,8 @@ def count_up(start, stop):
     for num in range(start,stop+1):
         print (num)
 
-count_up(5, 8)        
+count_up(5, 8)     
+
+
+from  convert import convert_temp
+
---
+++ Python/python-syntax/test.py
@@ -0,0 +1,5 @@
+scores = [1,2,3,34]
+
+print_scores =  list("apple")
+
+print(print_scores)
