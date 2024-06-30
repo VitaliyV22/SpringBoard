4a5a9a752e640caa49151c3c9f7b8d4c6dc9f745
VitaliyV22 <vitanskyyv@gmail.com>
2024-06-29 23:54:11 -0500

    finished express biztime

---
+++ Express/express-biztime/app.js
@@ -0,0 +1,39 @@
+/** BizTime express application. */
+
+
+const express = require("express");
+
+const app = express();
+const ExpressError = require("./expressError")
+
+app.use(express.json());
+app.use('/invoices', invoicesRoutes);
+app.use(express.json());
+app.use('/companies', companiesRoutes);
+
+/** 404 handler */
+
+
+app.use(function(req, res, next) {
+  const err = new ExpressError("Not Found", 404);
+  return next(err);
+});
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+const invoicesRoutes = require('./routes/invoices');
+
+module.exports = app;
